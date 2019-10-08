acd64274a05fe0c4cbebe695b843a5894ebabe2c
Twynzen <razzziel001@gmail.com>
2019-10-07 23:29:17 -0500

    ŕobando el codigo de tkinter

--- ventana.py
+++ ventana.py
@@ -1,16 +1,27 @@
-import tkinter
-from tkinter import filedialog
 
-raiz = tkinter.Tk()
-raiz.title("Mi programa")
+from tkinter import *
 
-#fondo
-frame = tkinter.Frame(raiz)
-frame.config(bg="Black",width=400,height=300)
-frame.pack()
+ventana = Tk()
+ventana.geometry("300x200")
+ventana.title("Menus")
 
-tituloMenu = "Bienvenido a mi primer programa"
-etiquetaLabel = tkinter.Label(frame, text=tituloMenu)
-etiquetaLabel.pack()
+def Funcion():
+    print ("Funcion Ejecutada por usuario usando el menu")
 
-raiz.mainloop()
+menu_general = Menu(ventana) #incrustamos un menu en la ventana, aunque todabia no es visible
+ventana["menu"] = menu_general #hacemos que la ventana tenga de menu a "menu_general"
+
+menu_archivo = Menu(menu_general) #menu archivo
+menu_archivo.add_command(label = "Abrir archivo", command = Funcion)
+menu_archivo.add_command(label = "Abrir carpeta", command = Funcion)
+menu_archivo.add_command(label = "Buscar archivos", command = Funcion)
+menu_archivo.add_separator()
+menu_archivo.add_command(label = "Salir", command = ventana.destroy)
+menu_general.add_cascade(label = "Archivo", menu = menu_archivo)
+
+menu_ayuda = Menu(menu_general) #menu ayuda
+menu_ayuda.add_command(label = "Acerca de...", command = Funcion)
+menu_ayuda.add_command(label = "Visit my site", command = Funcion)
+menu_general.add_cascade(label = "Ayuda", menu = menu_ayuda)
+
+ventana.mainloop() #bucle del programa
